cec12314cc329f6189b9b4d24f68bfa038486101
dawnos <flexmain@gmail.com>
2018-03-24 20:17:37 +0800

    bak

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 sudo cp ${BASE_DIR}/sources.list.tsinghua /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install language-pack-en language-pack-zh-hans
 
 sh ${BASE_DIR}/common/get-docker.sh
 
