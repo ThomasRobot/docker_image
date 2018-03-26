681768bd8d6d5cca0617a72be6c7beb45ca0c4be
dawnos <flexmain@gmail.com>
2018-03-26 14:36:22 +0800

    bak

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+CATKIN_WS=""
+if [ -d $HOME/catkin_ws ]; then
+  CATKIN_WS="-v $HOME/catkin_ws:/root/catkin_ws"
+fi
+
 docker run -it \
         -d \
         --privileged \
         --name thomas_os \
         -e DISPLAY=":0" \
         -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-        -v /media:/media \
-        -v /etc/localtime:/etc/localtime:ro \
         --net host \
         -w /root \
+        -v /media:/media \
+        -v /etc/localtime:/etc/localtime:ro \
         -v /dev:/dev \
+        ${CATKIN_WS} \
         thomas:v2 \
         roscore
 
