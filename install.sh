3b3be62d471bc0d6cd5ec88b1b6c88d5b223fbdc
dawnos <flexmain@gmail.com>
2018-03-27 13:27:30 +0800

    bak

--- install.sh
+++ install.sh
@@ -52,14 +52,17 @@ fi
 # Prepare startup script
 echo '
 if [ -z `docker container ls -f name=thomas_os --format {{.ID}}` ]; then
-  read -t 5 -n 1 -p "Start docker? (Y/n) " confirm
+  read -t 5 -n 1 -p "start docker? (Y/n) " confirm
+  confirm=${confirm:-Y}
   echo ''
-  if [ ! $confirm = 'n' ] && [ ! $confirm = 'N' ]; then
-    sh ~/docker_image/stop.sh 
+  if [ $confirm != 'n' ] && [ $confirm != 'N' ]; then
+    sh ~/docker_image/stop.sh
     sh ~/docker_image/start.sh
+    sh ~/docker_image/into.sh
   fi
 else
-  read -t 5 -n 1 -p "Enter docker? (y/N) " confirm
+  read -t 5 -n 1 -p "enter docker? (y/N) " confirm
+  confirm=${confirm:-N}
   echo ''
   if [ $confirm = 'y' ] || [ $confirm = 'Y' ]; then
     sh ~/docker_image/into.sh
