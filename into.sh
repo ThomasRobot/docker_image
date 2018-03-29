217e70a6962785bb57d40c2722d9a9f14da8721e
dawnos <flexmain@gmail.com>
2018-03-29 12:10:17 +0800

    bak

--- into.sh
+++ into.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xhost +local:root 1>/dev/null 2>&1
-docker exec -it -u ${USER} thomas_os /bin/bash
+docker exec -it -u ${USER} thomas_os /bin/bash -c "export COLUMNS=`tput cols`; export LINES=`tput lines`; exec bash"
 xhost -local:root 1>/dev/null 2>&1
