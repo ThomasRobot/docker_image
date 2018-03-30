0b221f0573032038c3645dcdbfdbef2585641f73
dawnos <flexmain@gmail.com>
2018-03-30 09:59:50 +0800

    rerange

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-docker build --network host -t thomas:$1 -f $1/Dockerfile . 
+# docker build --network host -t thomas:$1 -f $1/Dockerfile . 
+docker build --network host -t thomas:$1 $1
