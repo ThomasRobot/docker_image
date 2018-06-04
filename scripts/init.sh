4efbbe60f0dc63cf6abb2f0f614b96b09609e333
dawnos <litang.cv@gmail.com>
2018-06-04 18:35:19 +0800

    bak

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,7 +10,18 @@ if [ -n "${DOCKER_USER}" ] && [ -n "${DOCKER_USER_ID}" ] && [ -n "${DOCKER_GRP}"
     usermod -aG ${PG_GRP} "${DOCKER_USER}"
   fi
   echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-  sed "s/root/home\/${DOCKER_USER}/" ~/.zshrc > /home/${DOCKER_USER}/.zshrc
+  sed "s/root/home\/${DOCKER_USER}/" ~/.zshrc | grep -v catkin_ws > /home/${DOCKER_USER}/.zshrc
+  echo '
+if [ -n "$(find catkin_ws/src/* -type d)" ]; then
+  if [ -f ~/catkin_ws/devel/setup.sh ]; then
+    source ~/catkin_ws/devel/setup.sh
+  fi
+else
+  if [ -f ~/catkin_ws/install/setup.sh ]; then
+    source ~/catkin_ws/install/setup.sh
+  fi
+fi
+  ' >> /home/${DOCKER_USER}/.zshrc
   cp -r ~/.oh-my-zsh /home/${DOCKER_USER}
   cp ~/.bashrc /home/${DOCKER_USER}
   mkdir /home/${DOCKER_USER}/.ros
