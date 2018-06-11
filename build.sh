5a77d1bef9fa24ab615f67ded0c0010e1b50812f
dawnos <flexmain@gmail.com>
2018-06-11 11:12:00 +0800

    merge v1 v2

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 # docker build --network host -t thomas:$1 -f $1/Dockerfile . 
-proxychains docker build --network host -t thomas:$1 $1
+docker build --network host -t thomas:$1 $1
