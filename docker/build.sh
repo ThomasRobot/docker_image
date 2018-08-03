280664e7307a01c84dd9aff6e5df63ca30f6f378
dawnos <flexmain@gmail.com>
2018-08-03 19:03:06 +0800

    bak

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+BUILD_ARGS="--network host"
 
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-# docker build --network host -t thomas:$1 -f $1/Dockerfile . 
-docker build --network host -t thomas:$1 $1
+
+
+if [ $# -eq 0 ]; then
+  for image in `ls -d ${BASE_DIR}/*/`; do
+  image=$(echo ${image} | rev | cut -d'/' -f2 | rev)
+  echo $image
+    if [ -f "${BASE_DIR}/${image}/Dockerfile" ]; then
+      docker build ${BUILD_ARGS} -t thomas:${image} ${image}
+    fi
+  done
+else
+  docker build ${BUILD_ARGS} -t thomas:$1 $1
+fi
