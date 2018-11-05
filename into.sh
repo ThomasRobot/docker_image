7a3a0d1eb25819a370397b96096c0957a42a7302
dawnos <flexmain@gmail.com>
2018-11-05 10:42:00 +0800

    update

--- into.sh
+++ into.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 xhost +local:root 1>/dev/null 2>&1
+
+wait_cnt=5
+while [ $wait_cnt -gt 0 ]; do
+  ret=$(docker exec thomas_os /bin/zsh -c "cat /etc/passwd | cut -d':' -f1 | grep thomas")
+  if [ -n "$ret" ]; then
+    break
+  fi
+  i=$[$i-1]
+  sleep 1
+done
+
 docker exec -it -u ${USER} thomas_os /bin/zsh -c "export COLUMNS=`tput cols`; export LINES=`tput lines`; exec zsh"
 xhost -local:root 1>/dev/null 2>&1
